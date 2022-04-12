68905d9e09fa5d2b02ceb1d0dc26c7b7f56dfc3e
enofeb <eneszor95@gmail.com>
2022-04-12 17:16:17 +0300

    Chapter three example was updated

--- app/src/main/java/com/enofeb/composebooktraningzone/chapterOne/ChapterThree.kt
+++ app/src/main/java/com/enofeb/composebooktraningzone/chapterOne/ChapterThree.kt
@@ -1,21 +1,25 @@
 package com.enofeb.composebooktraningzone.chapterOne
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ColorPicker(color: MutableState<Color>) {
@@ -41,7 +45,7 @@ fun ColorScreen() {
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val color = remember {  mutableStateOf(Color.Magenta) }
+        val color = remember { mutableStateOf(Color.Magenta) }
         ColorPicker(color = color)
         Text(
             modifier = Modifier
@@ -54,4 +58,29 @@ fun ColorScreen() {
             )
         )
     }
+}
+
+@Composable
+fun OrderDemo() {
+    var color by remember { mutableStateOf(Color.Blue) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+            .border(BorderStroke(width = 2.dp, color = color))
+            .background(Color.LightGray)
+            .clickable {
+                color = if (color == Color.Blue)
+                    Color.Red
+                else
+                    Color.Blue
+            }
+    )
+}
+
+@Preview
+@Composable
+fun ThreePreview(){
+    OrderDemo()
 }
